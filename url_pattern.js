ef384c3a5bb71124013c85972d59673e204a625f
swdyh <youhei@gmail.com>
2009-04-27 13:51:44 +0900

    first commit

---
+++ url_pattern.js
@@ -0,0 +1,101 @@
+// http://dev.chromium.org/developers/design-documents/extensions/match-patterns
+
+function URLPattern(patterns) {
+    var hostTree = { keys: {}, values: [] }
+    var filePatterns = []
+    for (var i = 0; i < patterns.length; i++) {
+        var ps = [].concat(patterns[i])
+        for (var j = 0; j < ps.length; j++) {
+            try {
+                var pt = URLPattern.parse(ps[j])
+                if (pt.scheme == 'file') {
+                    filePatterns.push([i, j])
+                }
+                else {
+                    URLPattern.addTree(hostTree, pt.host, [i, j])
+                }
+            }
+            catch(e) {
+                // console.log(e)
+            }
+        }
+    }
+    this.patterns = patterns
+    this.hostTree = hostTree
+    this.filePatterns = filePatterns
+}
+URLPattern.addTree = function(tree, host, val) {
+    var currentNode = tree
+    var tmp = host.split('.').reverse()
+    for (var i = 0; i < tmp.length; i++) {
+        if (!currentNode.keys[tmp[i]]) {
+            currentNode.keys[tmp[i]] = { keys: {}, values: [] }
+        }
+        if (i != (tmp.length - 1)) {
+            currentNode = currentNode.keys[tmp[i]]
+        }
+        else {
+            currentNode.keys[tmp[i]].values.push(val)
+        }
+    }
+    return tree
+}
+URLPattern.prototype.matches = function(url) {
+    try {
+        var result = []
+        var u = URLPattern.parse(url)
+        var hosts = (u.scheme == 'file') ? this.filePatterns :
+            URLPattern.searchTree(this.hostTree, u.host)
+        for (var i = 0; i < hosts.length; i++) {
+            var index = hosts[i][0]
+            var patternIndex = hosts[i][1]
+            var pt = URLPattern.parse([].concat(this.patterns[index])[patternIndex])
+            if (!pt.path || URLPattern.matchesPath(pt.path, u.path)) {
+                result.push(index)
+            }
+        }
+        return result
+    }
+    catch(e) {
+        // console.log(e)
+        return []
+    }
+}
+URLPattern.matchesPath = function(pattern, path) {
+    var escaped = pattern.replace(/[.+?|()\\[\\]{}\\\\]/g, '\\$1')
+    var re = new RegExp(escaped.replace('*', '.*'))
+    return re.test(path || '/')
+}
+URLPattern.searchTree = function(tree, host) {
+    var tmp = host.split('.').reverse()
+    var currentNode = tree
+    var result = []
+    for (var i = 0; i < tmp.length; i++) {
+        if (currentNode.keys['*']) {
+            Array.prototype.push.apply(result, currentNode.keys['*'].values)
+        }
+        if (currentNode.keys[tmp[i]]) {
+            Array.prototype.push.apply(result,
+                                       currentNode.keys[tmp[i]].values)
+            currentNode = currentNode.keys[tmp[i]]
+        }
+        else {
+            break
+        }
+    }
+    return result
+}
+URLPattern.parse = function(url) {
+    var re = /^(http|https|file|ftp):\/\/([^\/]+)?(\/[^ ]+)?/
+    var matched = url.match(re)
+    if (matched) {
+        return { scheme: matched[1], host: matched[2], path: matched[3] }
+    }
+    else {
+        throw 'not supported url'
+    }
+}
+URLPattern.matches = function(pattern, url) {
+    var up = new URLPattern([pattern])
+    return up.matches(url).length > 0
+}
---
+++ url_pattern_test.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    module('urlpattern')
+    test('parse', function() {
+        var parsedURL = URLPattern.parse('http://*.google.com/foo/bar.html')
+        equals(parsedURL.scheme, 'http')
+        equals(parsedURL.host, '*.google.com')
+        equals(parsedURL.path, '/foo/bar.html')
+
+        var parsedURL_ =  URLPattern.parse('file:///foo/bar.html')
+        equals(parsedURL_.scheme, 'file')
+        equals(parsedURL_.path, '/foo/bar.html')
+        ok(!parsedURL_.host)
+    })
+
+    test('matches', function() {
+        var patterns = [
+                        [
+                         'https://*.google.com/foo*bar',
+                         'http://example.org/foo/bar.html',
+                         ],
+                        ['http://*/*'],
+                        [
+                         'http://*/foo*',
+                         'file:///foo*',
+                         ],
+                        'http://127.0.0.1/*',
+                        'file:///foo/bar/*',
+                         ]
+
+        var up = new URLPattern(patterns)
+        var ms = up.matches('http://127.0.0.1/foo.html')
+        equals(ms[0], 1)
+        equals(ms[1], 2)
+        equals(ms[2], 3)
+
+         ok(URLPattern.matches('http://*/*',
+                               'http://example.org/foo/bar.html'))
+         ok(URLPattern.matches('http://*/*',
+                               'http://www.google.com/'))
+
+         ok(URLPattern.matches('http://*/foo*',
+                               'http://example.com/foo/bar.html'))
+         ok(URLPattern.matches('http://*/foo*',
+                               'http://www.google.com/foo'))
+
+         ok(URLPattern.matches('https://*.google.com/foo*bar',
+                               'http://www.google.com/foo/baz/bar'))
+         ok(URLPattern.matches('https://*.google.com/foo*bar',
+                               'http://docs.google.com/foobar'))
+
+         ok(URLPattern.matches('file:///foo*', 'file:///foo/bar.html'))
+         ok(URLPattern.matches('file:///foo*', 'file:///foo'))
+
+         ok(URLPattern.matches('http://127.0.0.1/*',
+                               'http://127.0.0.1/'))
+         ok(URLPattern.matches('http://127.0.0.1/*',
+                               'http://127.0.0.1/foo/bar.html'))
+
+         ok(URLPattern.matches('http://www.google.co.jp/*',
+                               'http://www.google.co.jp/search?q=hello&lr=lang_ja&ie=utf-8&oe=utf-8&aq=t&rls=org.mozilla:ja-JP-mac:official&client=firefox-a'))
+    })
+})
